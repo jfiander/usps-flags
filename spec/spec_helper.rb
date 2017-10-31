5e461c68b9b18483893069fb0e1b0f38d1bbb5c1
Julian Fiander <julian@fiander.one>
2017-10-31 15:30:04 -0400

    Remove unnecessary requires in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,14 +3,7 @@ Coveralls.wear!
 require 'bundler/setup'
 Bundler.setup
 
-require 'usps_flags' # Contains direct SVG matching
-# require 'fileutils'
-# require 'zip'
-# require 'mini_magick'
-require 'usps_flags/config'
-require 'usps_flags/helpers'
-require 'usps_flags/core' # Contains direct SVG matching
-require 'usps_flags/generate'
+require 'usps_flags'
 
 RSpec.configure do |config|
   # some (optional) config here
