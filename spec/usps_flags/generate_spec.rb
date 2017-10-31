852d6b550311a64cefc4f66f158d7817e8b83f84
Julian Fiander <julian@fiander.one>
2017-10-31 15:29:50 -0400

    Split generate_spec into multiple examples

--- spec/usps_flags/generate_spec.rb
+++ spec/usps_flags/generate_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 
 describe USPSFlags::Generate do
-  it "should get the correct flag" do
+  it "should generate a flag with the correct size" do
     expect(USPSFlags::Generate.get("LtC", outfile: "")).to include("width=\"1024pt\" height=\"682pt\" viewBox=\"0 0 3072 2048\"")
+  end
+
+  it "should generate a flag with the correct field" do
     expect(USPSFlags::Generate.get("LtC", outfile: "")).to include(
       <<~SVG
         <path d="M 0 0
@@ -13,7 +16,13 @@ describe USPSFlags::Generate do
         " fill="#BF0D3E" />
       SVG
     )
+  end
+
+  it "should generate a flag with the correct starting position" do
     expect(USPSFlags::Generate.get("LtC", outfile: "")).to include("<path d=\"M 1536 512")
+  end
+
+  it "should generate a flag with the correct trident transformations" do
     expect(USPSFlags::Generate.get("LtC", outfile: "")).to include("<g transform=\"translate(-512)\">")
     expect(USPSFlags::Generate.get("LtC", outfile: "")).to include("<g transform=\"translate(512)\">")
   end
