c92a168889ed88f536a03aced6caa97c96184885
Julian Fiander <julian@fiander.one>
2017-11-03 16:13:09 -0400

    Add specs for special flags
    Also fixes one line length

--- spec/usps_flags/generate_spec.rb
+++ spec/usps_flags/generate_spec.rb
@@ -2,7 +2,9 @@ require 'spec_helper'
 
 describe USPSFlags::Generate do
   it "should generate a flag with the correct size" do
-    expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("width=\"1024pt\" height=\"682pt\" viewBox=\"0 0 3072 2048\"")
+    expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include(
+      "width=\"1024pt\" height=\"682pt\" viewBox=\"0 0 3072 2048\""
+    )
   end
 
   it "should generate a flag with the correct field" do
@@ -25,5 +27,35 @@ describe USPSFlags::Generate do
   it "should generate a flag with the correct trident transformations" do
     expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("<g transform=\"translate(-512)\">")
     expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("<g transform=\"translate(512)\">")
+  end
+
+  describe "special flags" do
+    it "should generate PortCap" do
+      expect(USPSFlags::Generate.svg("PortCap", outfile: "")).to include("<title>PORTCAP</title>")
+    end
+
+    it "should generate FleetCap" do
+      expect(USPSFlags::Generate.svg("FleetCap", outfile: "")).to include("<title>FLEETCAP</title>")
+    end
+
+    it "should generate DAide" do
+      expect(USPSFlags::Generate.svg("DAide", outfile: "")).to include("<title>DAIDE</title>")
+    end
+
+    it "should generate NAide" do
+      expect(USPSFlags::Generate.svg("NAide", outfile: "")).to include("<title>NAIDE</title>")
+    end
+
+    it "should generate FLt" do
+      expect(USPSFlags::Generate.svg("FLt", outfile: "")).to include("<title>FLT</title>")
+    end
+
+    it "should generate DFLt" do
+      expect(USPSFlags::Generate.svg("DFLt", outfile: "")).to include("<title>DFLT</title>")
+    end
+
+    it "should generate NFLt" do
+      expect(USPSFlags::Generate.svg("NFLt", outfile: "")).to include("<title>NFLT</title>")
+    end
   end
 end
