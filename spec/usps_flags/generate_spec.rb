cabaab5de10cb8ffbb068973b3f5181f4db135d8
Julian Fiander <julian@fiander.one>
2017-11-06 14:42:47 -0500

    Nest general features inside a describe block

--- spec/usps_flags/generate_spec.rb
+++ spec/usps_flags/generate_spec.rb
@@ -1,32 +1,34 @@
 require 'spec_helper'
 
 describe USPSFlags::Generate do
-  it "should generate a flag with the correct size" do
-    expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include(
-      "width=\"1024pt\" height=\"682pt\" viewBox=\"0 0 3072 2048\""
-    )
-  end
+  describe "general features" do
+    it "should generate a flag with the correct size" do
+      expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include(
+        "width=\"1024pt\" height=\"682pt\" viewBox=\"0 0 3072 2048\""
+      )
+    end
 
-  it "should generate a flag with the correct field" do
-    expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include(
-      <<~SVG
-        <path d="M 0 0
-          l 3072 0
-          l 0 2048
-          l -3072 0
-          l 0 -2048
-        " fill="#BF0D3E" />
-      SVG
-    )
-  end
+    it "should generate a flag with the correct field" do
+      expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include(
+        <<~SVG
+          <path d="M 0 0
+            l 3072 0
+            l 0 2048
+            l -3072 0
+            l 0 -2048
+          " fill="#BF0D3E" />
+        SVG
+      )
+    end
 
-  it "should generate a flag with the correct starting position" do
-    expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("<path d=\"M 1536 512")
-  end
+    it "should generate a flag with the correct starting position" do
+      expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("<path d=\"M 1536 512")
+    end
 
-  it "should generate a flag with the correct trident transformations" do
-    expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("<g transform=\"translate(-512)\">")
-    expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("<g transform=\"translate(512)\">")
+    it "should generate a flag with the correct trident transformations" do
+      expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("<g transform=\"translate(-512)\">")
+      expect(USPSFlags::Generate.svg("LtC", outfile: "")).to include("<g transform=\"translate(512)\">")
+    end
   end
 
   describe "special flags" do
