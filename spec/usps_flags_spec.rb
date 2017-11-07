58e2f3ab6175d62b7480cf85989702806316818b
Julian Fiander <julian@fiander.one>
2017-11-07 11:44:53 -0500

    Add context for with png_file set

--- spec/usps_flags_spec.rb
+++ spec/usps_flags_spec.rb
@@ -144,9 +144,15 @@ describe USPSFlags do
           expect { @flag.png }.to raise_error(USPSFlags::Errors::PNGGenerationError, "A path must be set with png_file.")
         end
 
-        it "should not raise PNGGenerationError with png_file set" do
-          @flag.png_file = "#{$tmp_alt_flags_dir}/PNG/LtC.png"
-          expect { @flag.png }.to_not raise_error(USPSFlags::Errors::PNGGenerationError)
+        context "with png_file set" do
+          before(:each) do
+            @flag.png_file = "#{$tmp_alt_flags_dir}/PNG/LtC.png"
+            ::FileUtils.mkdir_p("#{$tmp_alt_flags_dir}/PNG/")
+          end
+
+          it "should not raise PNGGenerationError with png_file set" do
+            expect { @flag.png }.to_not raise_error(USPSFlags::Errors::PNGGenerationError)
+          end
         end
       end
     end
