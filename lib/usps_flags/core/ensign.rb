138320bbcac04aa037aeed12966cfc17a986852b
Julian Fiander <julian@fiander.one>
2017-11-06 22:17:51 -0500

    Split Core::Ensign into smaller methods

--- lib/usps_flags/core/ensign.rb
+++ lib/usps_flags/core/ensign.rb
@@ -4,7 +4,22 @@
 # @private
 class USPSFlags::Core::Ensign
   def svg
-    field = <<~SVG
+    <<~SVG
+      #{stripes}
+      <g transform="scale(0.3675)">
+        <g transform="translate(1200, 600)">
+          <g transform="rotate(-45, 693, 1500)">
+            #{USPSFlags::Core::Anchor.new(color: :white).svg}
+          </g>
+        </g>
+      </g>
+      #{star_circle}
+    SVG
+  end
+
+  private
+  def stripes
+    <<~SVG
       <rect x="0" y="0" width="#{USPSFlags::Config::BASE_FLY/13}" height="#{USPSFlags::Config::BASE_HOIST}" fill="#{USPSFlags::Config::BLUE}" />
       <rect x="#{USPSFlags::Config::BASE_FLY*1/13}" y="0" width="#{USPSFlags::Config::BASE_FLY/13}" height="#{USPSFlags::Config::BASE_HOIST}" fill="#FFFFFF" />
       <rect x="#{USPSFlags::Config::BASE_FLY*2/13}" y="0" width="#{USPSFlags::Config::BASE_FLY/13}" height="#{USPSFlags::Config::BASE_HOIST}" fill="#{USPSFlags::Config::BLUE}" />
@@ -21,13 +36,13 @@ class USPSFlags::Core::Ensign
       <rect x="0" y="0" width="#{USPSFlags::Config::BASE_FLY*6/13}" height="1000" fill="#{USPSFlags::Config::RED}" />
     SVG
     # <circle cx="#{USPSFlags::Config::BASE_FLY*3/13}" cy="#{USPSFlags::Config::BASE_HOIST/4}" r="#{USPSFlags::Config::BASE_FLY*6/13*5/16}" fill="#FFFFFF" fill-opacity="0.6" />
+  end
 
-    anchor = USPSFlags::Core::Anchor.new(color: :white).svg
-
-    star_circle = ""
+  def star_circle
+    svg = ""
     (0..13).each do |i|
       rotation = i * (360.0 / 13)
-      star_circle << <<~SVG
+      svg << <<~SVG
         <g transform="scale(0.375)">
           <g transform="translate(1885, 465)">
             <g transform="rotate(#{rotation}, 0, 900)">
@@ -37,17 +52,6 @@ class USPSFlags::Core::Ensign
         </g>
       SVG
     end
-
-    <<~SVG
-      #{field}
-      <g transform="scale(0.3675)">
-        <g transform="translate(1200, 600)">
-          <g transform="rotate(-45, 693, 1500)">
-            #{anchor}
-          </g>
-        </g>
-      </g>
-      #{star_circle}
-    SVG
+    svg
   end
 end
