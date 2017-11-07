491a3d077a02b09b068cdf29a4879705561d2af3
Julian Fiander <julian@fiander.one>
2017-11-07 00:01:32 -0500

    Add spec to Rational

--- spec/rational_spec.rb
+++ spec/rational_spec.rb
@@ -6,6 +6,10 @@ describe Rational do
       expect(Rational(4,3).to_simplified_a).to eql([1, Rational(1,3)])
     end
 
+    it "should render a proper fraction as String" do
+      expect(Rational(2,3).to_simplified_a).to eql("2/3")
+    end
+
     it "should render a simplified fraction as a String" do
       expect(Rational(4,3).to_simplified_s).to eql("1 1/3")
     end
