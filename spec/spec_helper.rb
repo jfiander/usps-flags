268f8d7ac0a1b3d8864b3c9933c33c59bc1618c5
Julian Fiander <julian@fiander.one>
2017-11-07 20:08:50 -0500

    Clear up variable name

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,8 +16,8 @@ RSpec.configure do |config|
     $tmp_flags_dir = "tmp/flags"
     $tmp_alt_flags_dir = "tmp/alt_flags"
 
-    USPSFlags::Config.new do |config|
-      config.flags_dir = $tmp_flags_dir
+    USPSFlags::Config.new do |c|
+      c.flags_dir = $tmp_flags_dir
     end
   end
 
