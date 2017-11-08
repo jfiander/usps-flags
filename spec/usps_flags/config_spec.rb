4a471cae44f22ab797df028d3dfa51764e478384
Julian Fiander <julian@fiander.one>
2017-11-08 16:01:44 -0500

    Reorganize specs

--- spec/usps_flags/config_spec.rb
+++ spec/usps_flags/config_spec.rb
@@ -1,28 +1,25 @@
 require 'spec_helper'
 
 describe USPSFlags::Config do
-  describe "flags_dir" do
+  describe "class variable accessors" do
     it "should return the current flags directory" do
       expect(USPSFlags::Config.flags_dir).to eql($tmp_flags_dir)
     end
-  end
 
-  describe "log_path" do
     it "should return the current flags directory" do
       default_log_path = $tmp_flags_dir + "/log"
       expect(USPSFlags::Config.log_path).to eql(default_log_path)
     end
+
+    it "should return a Boolean from use_larger_tridents" do
+      expect([true, false]).to include(USPSFlags::Config.use_larger_tridents)
+    end
+
   end
 
   describe "trident" do
     it "should return a Hash from trident" do
       expect(USPSFlags::Config.trident).to be_a(Hash)
-    end
-  end
-
-  describe "use_larger_tridents" do
-    it "should return a Boolean from use_larger_tridents" do
-      expect([true, false]).to include(USPSFlags::Config.use_larger_tridents)
     end
   end
 
