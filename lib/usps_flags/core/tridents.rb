55a128c52a1fa154cadb7c1fc76bb44a67419c12
Julian Fiander <jsfiander@gmail.com>
2020-02-17 00:49:18 -0500

    Fix CC and VC rotation points
    Corrects actual rotation points
    Simplifies math to rotate tridents
    Also move and update comments

--- lib/usps_flags/core/tridents.rb
+++ lib/usps_flags/core/tridents.rb
@@ -8,30 +8,47 @@ class USPSFlags
   class Core
     class Tridents
       class << self
+        # The side C/C tridents are angled 45 degrees, and intersect the central one at 1/3 up from the bottom
+        #
+        # Note: translate(132, 130) corrects the center of rotation for each angled trident due to the (transparent)
+        #   field size of the include trident SVG
+        #
+        # Center of rotation:
+        # # radius = USPSFlags.configuration.trident[:bar_width] / 2
+        # <circle cx="#{cc_center[:x]}" cy="#{cc_center[:y]}" r="#{radius}" fill="red" />
         def cc(type, trident_color:)
-          # The side C/C tridents are angled 45 degrees, and intersect the central one at 1/3 up from the bottom
           trident = USPSFlags::Core.trident(type, color: trident_color)
-          x_distance = USPSFlags::Config::BASE_FLY * 4 / 39
-          y_distance = USPSFlags::Config::BASE_FLY * 5 / 78
           <<~SVG
-            <g transform="translate(-#{x_distance}, #{y_distance})"><g transform="rotate(-45, #{USPSFlags::Config::BASE_FLY / 2}, #{USPSFlags::Config::BASE_HOIST / 2})">\n#{trident}</g></g>
-            \n#{trident}
-            <g transform="translate(#{x_distance}, #{y_distance})"><g transform="rotate(45, #{USPSFlags::Config::BASE_FLY / 2}, #{USPSFlags::Config::BASE_HOIST / 2})">\n#{trident}</g></g>
+            <g transform="translate(132, 130)">
+              <g transform="rotate(-45, #{cc_center[:x]}, #{cc_center[:y]})">
+                #{trident}
+              </g>
+              <g transform="rotate(45, #{cc_center[:x]}, #{cc_center[:y]})">
+                #{trident}
+              </g>
+            </g>
+            #{trident}
           SVG
         end
 
+        # V/C tridents are angled 45 degrees, and intersect at 15/32 up from the bottom
+        #
+        # Note: translate(132, 50) corrects the center of rotation for each angled trident due to the (transparent)
+        #   field size of the include trident SVG
         def vc(type, trident_color:)
-          # V/C tridents are angled 45 degrees, and intersect at 15/32 up from the bottom
           trident = USPSFlags::Core.trident(type, color: trident_color)
-          x_distance = USPSFlags::Config::BASE_FLY * 4 / 55
           <<~SVG
-            <g transform="translate(-#{x_distance})"><g transform="rotate(-45, #{USPSFlags::Config::BASE_FLY / 2}, #{USPSFlags::Config::BASE_HOIST / 2})">\n#{trident}</g></g>
-            <g transform="translate(#{x_distance})"><g transform="rotate(45, #{USPSFlags::Config::BASE_FLY / 2}, #{USPSFlags::Config::BASE_HOIST / 2})">\n#{trident}</g></g>
+            <g transform="translate(132, 50) rotate(-45, #{vc_center[:x]}, #{vc_center[:y]})">
+              #{trident}
+            </g>
+            <g transform="translate(132, 50) rotate(45, #{vc_center[:x]}, #{vc_center[:y]})">
+              #{trident}
+            </g>
           SVG
         end
 
+        # Cdr and D/C tridents are spaced 1/2 the fly apart with the central one 1/4 the fly above the sides
         def three(type, trident_color:, field_color:)
-          # Cdr and D/C tridents are spaced 1/2 the fly apart with the central one 1/4 the fly above the sides
           trident = USPSFlags::Core.trident(type, color: trident_color, field_color: field_color)
           x_distance = USPSFlags::Config::BASE_FLY / 4
           y_distance = USPSFlags::Config::BASE_FLY / 16
@@ -42,8 +59,8 @@ class USPSFlags
           SVG
         end
 
+        # Lt/C and D/Lt/C tridents are spaced 1/3 the fly apart
         def two(type, trident_color:, field_color:)
-          # Lt/C and D/Lt/C tridents are spaced 1/3 the fly apart
           trident = USPSFlags::Core.trident(type, color: trident_color, field_color: field_color)
           x_distance = USPSFlags::Config::BASE_FLY / 6
           <<~SVG
@@ -52,14 +69,37 @@ class USPSFlags
           SVG
         end
 
+        # Swallowtail tridents need to move towards the hoist due to the tails
         def offset(type, field_color:, field: true)
-          # Swallowtail tridents need to move towards the hoist due to the tails
           x_distance = USPSFlags::Config::BASE_FLY / 10 if field
           svg = +''
           svg << "<g transform=\"translate(-#{x_distance})\">" if field
           svg << USPSFlags::Core.trident(type, field_color: field_color, color: :red)
           svg << '</g>' if field
           svg
+        end
+
+      private
+
+        def rotation_center(height_fraction)
+          {
+            x: USPSFlags::Config::BASE_FLY / 2,
+            y: (USPSFlags::Config::BASE_HOIST * 7 / 8) - (n_staff_height * height_fraction)
+          }
+        end
+
+        def cc_center
+          rotation_center(Rational(1, 3))
+        end
+
+        def vc_center
+          rotation_center(Rational(15, 32))
+        end
+
+        def n_staff_height
+          top_height = USPSFlags.configuration.trident[:crossbar_from_top]
+          bars = (USPSFlags.configuration.trident[:bar_width] * 3)
+          USPSFlags.configuration.trident_heights[:n] - top_height - bars
         end
       end
     end
